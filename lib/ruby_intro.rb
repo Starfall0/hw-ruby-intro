f4ad4480554c52c7a0ae20ce7e720c5c67d0f4fc
Starfall0 <theradeth@gmail.com>
2024-01-05 04:24:25 +0000

    Add existing project files to Git

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,47 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  arr.sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  arr.sort.last(2).sum # sort the array and get the sum of last two elements
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  arr.combination(2).any? { |a, b| a + b == n } # use combination to check if two number of array is equal to n(use a Bloack as condition)
 end
 
-# Part 2
+# Part 2A
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  /^[^aeiou\W]/i.match?(s) # use a regular expression to check if the string starts with a consonant(aeiou is vowel) ^ negate the character set
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return false unless /^[01]+$/.match(s) # return false if the string did not contain 0 or 1
+  num = s.to_i(2) # convert from string to integer(2 is binary)
+  num % 4 == 0 # check if value is a multiple of 4
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  attr_accessor :isbn, :price
+
+  def initialize(isbn, price)
+    raise ArgumentError, 'Invalid ISBN number' if isbn.empty? # check if isbn is emty then error
+    raise ArgumentError, 'Price must be greater than zero' if price <= 0 # check if the price is lower than zero then error
+
+    @isbn = isbn
+    @price = price
+  end
+
+  def price_as_string
+    format('$%.2f', @price) # formats the price with two decimal places and adds a dollar sign in front
+  end
 end
